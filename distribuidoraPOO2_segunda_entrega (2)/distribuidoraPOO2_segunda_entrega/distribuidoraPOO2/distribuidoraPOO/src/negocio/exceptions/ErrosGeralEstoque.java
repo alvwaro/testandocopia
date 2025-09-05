c5bcb2350efb3316717e7d0111f2777bdd5d8a11
Álvaro <drawvaro@gmail.com>
2025-09-04 22:01:40 -0300

    Delete distribuidoraPOO2_segunda_entrega (2) directory

--- distribuidoraPOO2_segunda_entrega (2)/distribuidoraPOO2_segunda_entrega/distribuidoraPOO2/distribuidoraPOO/src/negocio/exceptions/ErrosGeralEstoque.java
+++
@@ -1,7 +0,0 @@
-package negocio.exceptions;
-
-public class ErrosGeralEstoque extends EstoqueErros{
-    public ErrosGeralEstoque(String mensagem) {
-        super(mensagem);
-    }
-}
--- distribuidoraPOO2_segunda_entrega (2)/distribuidoraPOO2_segunda_entrega/distribuidoraPOO2/distribuidoraPOO/src/negocio/exceptions/ProdutoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package negocio.exceptions;
-
-public class ProdutoNaoEncontradoException extends EstoqueErros{
-    public ProdutoNaoEncontradoException(String mensagem) {
-        super(mensagem);
-    }
-}
